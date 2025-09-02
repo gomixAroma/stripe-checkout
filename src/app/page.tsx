464534684f3acb9ccd490c37fc6d4865a0a96791
Aroma Yoshida <xj665488@gmail.com>
2025-09-02 16:10:53 +0900

    fix:useSearchParamsをSuspenseで囲うように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
+import { Suspense } from 'react';
 export default function Home() {
   return (
-    <>
+    <Suspense fallback="loading...">
       <Box
         sx={{
           display: 'grid',
@@ -74,6 +75,6 @@ export default function Home() {
         </Box>
       </Box>
       <Error />
-    </>
+    </Susp>
   );
 }
